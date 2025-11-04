7fdc14e74e161687c5a11ba3545bcd13f9e65c76
Nicholas Pneumaticos <nicholaspneu@gmail.com>
2025-11-04 15:01:11 +0000

    fixed tbd columns

--- main.py
+++ main.py
@@ -19,6 +19,24 @@ def main() -> None:
 	print(f"Usage: python3 ./main.py ({"|".join(SHEET_NAMES)})")
 
   elif sys.argv[1] == SHEET_NAMES[0]:
+    convert_tbd(SHEETS[0])
+
+def convert_tbd(sheet: Sheet) -> None:
+  columns = [
+    "construction_type_name",
+    "Wall Reference",
+    "description_x",
+    "Quality of Detail",
+    "u_w_per_m_k",
+    "description_y",
+    "Material ID",
+    "Multiplier",
+    "Unit",
+    "Reference"
+  ]
+  df = pd.read_excel(SHEET_PATH, sheet_name : sheet.sheet_name, skiprows = 1)
+  df.columns = columns
+
 
 if __name__ == "__main__":
   main()
